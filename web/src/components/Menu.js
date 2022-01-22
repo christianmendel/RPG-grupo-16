d0d11244854e45b8386fdf0c1bfd38f6472d7873
RafaelMeneses <rafaelp.m@hotmail.com>
2022-01-22 14:12:26 -0300

    LandingPage

--- web/src/components/Menu.js
+++ web/src/components/Menu.js
@@ -1,21 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import '../styles/global.css';
-import '../styles/pages/landing.css';
+import '../styles/components/menu.css';
 import background from '../img/background.png';
-import api from '../api/api'
 
-function Landing() {
+function Menu() {
 
     const [toggleState, setToggleState] = useState(1);
-    const [races, setRaces] = useState([]);
-
-    useEffect(() => {
-        api.get('racas').then(response=>{
-            setRaces(response.data)
-        })  
-
-        //eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])  
 
     const toggleTab = (index) => {
         setToggleState(index);
@@ -31,19 +21,16 @@ function Landing() {
                             className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
                             onClick={() => toggleTab(1)}
                         >
-                            Criar personagem
                         </button>
                         <button
                             className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
                             onClick={() => toggleTab(2)}
                         >
-                            Selecionar personagem
                         </button>
                         <button
                             className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
                             onClick={() => toggleTab(3)}
                         >
-                            Excluir personagem
                         </button>
                     </div>
                 </div>
@@ -51,28 +38,14 @@ function Landing() {
                 <div className="container container-tab">
                     <div className="content-tabs">
                         <div className={toggleState === 1 ? "content  active-content" : "content"} >
-                                <h2>Criação de personagem</h2>
-                                <label htmlFor="">Nome: </label>
-                                <input type="text"/>
-                                <label htmlFor="">Raça: </label>
-                                <select name="" id="">
-                                    {races?.map(item => <option key={item.id}> {item.breed} </option>)}
-                                </select>
-                                <label htmlFor="">Classe: </label>
-                                <select name="" id="">
-                                    {races?.map(item => <option key={item.id}> {item.breed} </option>)}
-                                </select>
-                                {/* <button onClick={testeDados}>BotaoDosDados</button> */}
+                                
                         </div>
                         <div className={toggleState === 2 ? "content  active-content" : "content"}>
-                            <h2>Personagens</h2>
-                            <h3>Anão</h3>
-                            <h3>Assassino</h3>
+                            
                         </div>
 
                         <div className={toggleState === 3 ? "content  active-content" : "content"}>
-                            <h2>Personagens</h2>
-                            <p>racas</p>
+                            
                         </div>
                     </div>
                 </div>
@@ -81,4 +54,4 @@ function Landing() {
     );
 }
 
-export default Landing;
+export default Menu;
